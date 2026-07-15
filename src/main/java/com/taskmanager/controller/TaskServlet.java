42d955908af9cec5b04eadbfc4ef5d7f6d9dc0b6
sleshamhrzn4 <mhrznslesha4@gmail.com>
2026-07-15 21:35:18 +0545

    Changes made in the TaskServlet

---
+++ src/main/java/com/taskmanager/controller/TaskServlet.java
@@ -0,0 +1,26 @@
+package com.taskmanager.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.Serial;
+
+    @WebServlet(asyncSupported = true,urlPatterns = {"/task"})
+    public class TaskServlet extends HttpServlet {
+        @Serial
+        private static final long serialVersionUID = 1L;
+
+        public TaskServlet() {
+            super();
+        }
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            req.getRequestDispatcher("/WEB-INF/pages/userdashboard.jsp").forward(req, resp);
+        }
+
+    }
--- src/main/webapp/WEB-INF/pages/userdashboard.jsp
+++ src/main/webapp/WEB-INF/pages/userdashboard.jsp
@@ -1,10 +1,4 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: A C E R
-  Date: 7/15/2026
-  Time: 8:37 PM
-  To change this template use File | Settings | File Templates.
---%>
+
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
